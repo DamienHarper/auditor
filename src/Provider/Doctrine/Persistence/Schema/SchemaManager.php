60aaac0afd6175a5c3794bf0c6604457f27b875f
damienharper <damien.harper@gmail.com>
2020-11-30 12:41:41 +0100

    Moved some platform specific code to a dedicated helper class.

--- src/Provider/Doctrine/Persistence/Schema/SchemaManager.php
+++ src/Provider/Doctrine/Persistence/Schema/SchemaManager.php
@@ -5,11 +5,11 @@ namespace DH\Auditor\Provider\Doctrine\Persistence\Schema;
 use DH\Auditor\Provider\Doctrine\Configuration;
 use DH\Auditor\Provider\Doctrine\DoctrineProvider;
 use DH\Auditor\Provider\Doctrine\Persistence\Helper\DoctrineHelper;
+use DH\Auditor\Provider\Doctrine\Persistence\Helper\PlatformHelper;
 use DH\Auditor\Provider\Doctrine\Persistence\Helper\SchemaHelper;
 use DH\Auditor\Provider\Doctrine\Service\AuditingService;
 use DH\Auditor\Provider\Doctrine\Service\StorageService;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Driver\ServerInfoAwareConnection;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\DBAL\Schema\Table;
@@ -181,7 +181,7 @@ class SchemaManager
             $auditTable = $schema->createTable($auditTablename);
 
             // Add columns to audit table
-            $isJsonSupported = $this->isJsonSupported($connection);
+            $isJsonSupported = PlatformHelper::isJsonSupported($connection);
             foreach (SchemaHelper::getAuditTableColumns() as $columnName => $struct) {
                 if (DoctrineHelper::getDoctrineType('JSON') === $struct['type'] && $isJsonSupported) {
                     $type = DoctrineHelper::getDoctrineType('TEXT');
@@ -201,7 +201,7 @@ class SchemaManager
                         [$columnName],
                         $struct['name'],
                         [],
-                        $this->isIndexLengthLimited($columnName, $connection) ? ['lengths' => [191]] : []
+                        PlatformHelper::isIndexLengthLimited($columnName, $connection) ? ['lengths' => [191]] : []
                     );
                 }
             }
@@ -242,7 +242,7 @@ class SchemaManager
     {
         $processed = [];
 
-        $isJsonSupported = $this->isJsonSupported($connection);
+        $isJsonSupported = PlatformHelper::isJsonSupported($connection);
         foreach ($columns as $column) {
             if (\array_key_exists($column->getName(), $expectedColumns)) {
                 // column is part of expected columns
@@ -294,121 +294,9 @@ class SchemaManager
                     [$columnName],
                     $options['name'],
                     [],
-                    $this->isIndexLengthLimited($columnName, $connection) ? ['lengths' => [191]] : []
+                    PlatformHelper::isIndexLengthLimited($columnName, $connection) ? ['lengths' => [191]] : []
                 );
             }
         }
-    }
-
-    /**
-     * MySQL < 5.7.7 and MariaDb < 10.2.2 index length requirements.
-     *
-     * @see https://github.com/doctrine/dbal/issues/3419
-     */
-    private function isIndexLengthLimited(string $name, Connection $connection): bool
-    {
-        $columns = SchemaHelper::getAuditTableColumns();
-        if (
-            !isset($columns[$name])
-            || $columns[$name]['type'] !== DoctrineHelper::getDoctrineType('STRING')
-            || (
-                isset($columns[$name]['options'], $columns[$name]['options']['length'])
-                && $columns[$name]['options']['length'] < 191
-            )
-        ) {
-            return false;
-        }
-
-        $version = $this->getServerVersion($connection);
-
-        if (null === $version) {
-            return false;
-        }
-
-        $mariadb = false !== mb_stripos($version, 'mariadb');
-        if ($mariadb && version_compare($this->getMariaDbMysqlVersionNumber($version), '10.2.2', '<')) {
-            return true;
-        }
-
-        if (!$mariadb && version_compare($this->getOracleMysqlVersionNumber($version), '5.7.7', '<')) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function getServerVersion(Connection $connection): ?string
-    {
-        $wrappedConnection = $connection->getWrappedConnection();
-
-        if ($wrappedConnection instanceof ServerInfoAwareConnection) {
-            return $wrappedConnection->getServerVersion();
-        }
-
-        return null;
-    }
-
-    private function isJsonSupported(Connection $connection): bool
-    {
-        $version = $this->getServerVersion($connection);
-        if (null === $version) {
-            return true;
-        }
-
-        $mariadb = false !== mb_stripos($version, 'mariadb');
-        if ($mariadb && version_compare($this->getMariaDbMysqlVersionNumber($version), '10.2.7', '<')) {
-            // JSON wasn't supported on MariaDB before 10.2.7
-            // @see https://mariadb.com/kb/en/json-data-type/
-            return false;
-        }
-
-        // Assume JSON is supported
-        return true;
-    }
-
-    /**
-     * Get a normalized 'version number' from the server string
-     * returned by Oracle MySQL servers.
-     *
-     * @param string $versionString Version string returned by the driver, i.e. '5.7.10'
-     *
-     * @copyright Doctrine team
-     */
-    private function getOracleMysqlVersionNumber(string $versionString): string
-    {
-        preg_match(
-            '/^(?P<major>\d+)(?:\.(?P<minor>\d+)(?:\.(?P<patch>\d+))?)?/',
-            $versionString,
-            $versionParts
-        );
-
-        $majorVersion = $versionParts['major'];
-        $minorVersion = $versionParts['minor'] ?? 0;
-        $patchVersion = $versionParts['patch'] ?? null;
-
-        if ('5' === $majorVersion && '7' === $minorVersion && null === $patchVersion) {
-            $patchVersion = '9';
-        }
-
-        return $majorVersion.'.'.$minorVersion.'.'.$patchVersion;
-    }
-
-    /**
-     * Detect MariaDB server version, including hack for some mariadb distributions
-     * that starts with the prefix '5.5.5-'.
-     *
-     * @param string $versionString Version string as returned by mariadb server, i.e. '5.5.5-Mariadb-10.0.8-xenial'
-     *
-     * @copyright Doctrine team
-     */
-    private function getMariaDbMysqlVersionNumber(string $versionString): string
-    {
-        preg_match(
-            '/^(?:5\.5\.5-)?(mariadb-)?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)/i',
-            $versionString,
-            $versionParts
-        );
-
-        return $versionParts['major'].'.'.$versionParts['minor'].'.'.$versionParts['patch'];
     }
 }
