febeea4a79821738da8b27c035eadf1f2f9a99d7
a.dmitryuk <a.dmitryuk@movavi.com>
2022-10-26 16:30:40 +0700

    cs-fixer

--- src/Provider/Doctrine/Auditing/Transaction/AuditTrait.php
+++ src/Provider/Doctrine/Auditing/Transaction/AuditTrait.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DH\Auditor\Provider\Doctrine\Auditing\Transaction;
 
+use BackedEnum;
 use DH\Auditor\Exception\MappingException;
 use DH\Auditor\Provider\Doctrine\Persistence\Helper\DoctrineHelper;
 use DH\Auditor\User\UserInterface;
@@ -75,7 +76,7 @@ trait AuditTrait
             return null;
         }
 
-        if (\PHP_VERSION_ID >= 81000 && $value instanceof \BackedEnum) {
+        if (\PHP_VERSION_ID >= 81000 && $value instanceof BackedEnum) {
             $value = $value->value;
         }
 
