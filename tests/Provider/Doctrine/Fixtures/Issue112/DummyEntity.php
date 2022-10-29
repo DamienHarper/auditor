b683695d1d8cdfeca385527593d539ae094d3bd6
a.dmitryuk <a.dmitryuk@movavi.com>
2022-10-29 20:59:20 +0700

    Dummy Entity

---
+++ tests/Provider/Doctrine/Fixtures/Issue112/DummyEntity.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DH\Auditor\Tests\Provider\Doctrine\Fixtures\Issue112;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="author")
+ */
+#[ORM\Entity]
+#[ORM\Table(name: 'author')]
+class DummyEntity
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", options={"unsigned": true})
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    #[ORM\Id]
+    #[ORM\Column(type: 'integer', options: ['unsigned' => true])]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    protected int $primaryKey;
+
+    public function getPrimaryKey(): int
+    {
+        return $this->primaryKey;
+    }
+
+    public function setPrimaryKey(int $primaryKey): void
+    {
+        $this->primaryKey = $primaryKey;
+    }
+}
