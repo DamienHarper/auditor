f5afb861ffe41fc43c18756f2a1ff280e22cfb0b
a.dmitryuk <a.dmitryuk@movavi.com>
2022-11-06 20:48:46 +0700

    use AbstractConnectionMiddleware

--- src/Provider/Doctrine/Auditing/Logger/Middleware/DHConnection.php
+++ src/Provider/Doctrine/Auditing/Logger/Middleware/DHConnection.php
@@ -5,52 +5,19 @@ declare(strict_types=1);
 namespace DH\Auditor\Provider\Doctrine\Auditing\Logger\Middleware;
 
 use Doctrine\DBAL\Driver\Connection as ConnectionInterface;
-use Doctrine\DBAL\Driver\Result;
-use Doctrine\DBAL\Driver\Statement;
-use Doctrine\DBAL\ParameterType;
+use Doctrine\DBAL\Driver\Middleware\AbstractConnectionMiddleware;
 
 /**
  * @interal
  */
-final class DHConnection implements ConnectionInterface
+final class DHConnection extends AbstractConnectionMiddleware
 {
-    private ConnectionInterface $connection;
     private DHDriver $DHDriver;
 
     public function __construct(ConnectionInterface $connection, DHDriver $DHDriver)
     {
-        $this->connection = $connection;
+        parent::__construct($connection);
         $this->DHDriver = $DHDriver;
-    }
-
-    public function prepare(string $sql): Statement
-    {
-        return $this->connection->prepare($sql);
-    }
-
-    public function query(string $sql): Result
-    {
-        return $this->connection->query($sql);
-    }
-
-    public function quote($value, $type = ParameterType::STRING)
-    {
-        return $this->connection->quote($value, $type);
-    }
-
-    public function exec(string $sql): int
-    {
-        return $this->connection->exec($sql);
-    }
-
-    public function lastInsertId($name = null)
-    {
-        return $this->connection->lastInsertId($name);
-    }
-
-    public function beginTransaction(): bool
-    {
-        return $this->connection->beginTransaction();
     }
 
     public function commit(): bool
@@ -61,21 +28,13 @@ final class DHConnection implements ConnectionInterface
         }
         $this->DHDriver->resetDHFlusherList();
 
-        return $this->connection->commit();
+        return parent::commit();
     }
 
     public function rollBack(): bool
     {
         $this->DHDriver->resetDHFlusherList();
 
-        return $this->connection->rollBack();
-    }
-
-    /**
-     * @return object|resource
-     */
-    public function getNativeConnection()
-    {
-        return $this->connection->getNativeConnection();
+        return parent::rollBack();
     }
 }
