55068293687ceb3a913b1afeb1732fb0839ae0d6
damienharper <damien.harper@gmail.com>
2022-12-02 18:58:50 +0100

    Removed annotations from cherrypicked commits

--- tests/Provider/Doctrine/Fixtures/Issue132/AbstractParentEntity.php
+++ tests/Provider/Doctrine/Fixtures/Issue132/AbstractParentEntity.php
@@ -6,13 +6,6 @@ namespace DH\Auditor\Tests\Provider\Doctrine\Fixtures\Issue132;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- * @ORM\Table("parent_entities")
- * @ORM\InheritanceType("SINGLE_TABLE")
- * @ORM\DiscriminatorColumn(name="type", type="string")
- * @ORM\DiscriminatorMap({"Dummy": DummyEntity::class})
- */
 #[ORM\Entity]
 #[ORM\Table('parent_entities')]
 #[ORM\InheritanceType('SINGLE_TABLE')]
@@ -20,11 +13,6 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\DiscriminatorMap(['Dummy' => DummyEntity::class])]
 abstract class AbstractParentEntity
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column
-     * @ORM\GeneratedValue
-     */
     #[ORM\Id]
     #[ORM\Column]
     #[ORM\GeneratedValue]
--- tests/Provider/Doctrine/Fixtures/Issue132/DummyEntity.php
+++ tests/Provider/Doctrine/Fixtures/Issue132/DummyEntity.php
@@ -6,9 +6,6 @@ namespace DH\Auditor\Tests\Provider\Doctrine\Fixtures\Issue132;
 
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- */
 #[ORM\Entity]
 class DummyEntity extends AbstractParentEntity
 {
