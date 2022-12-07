be036cec8073acbd4c3bbcecf091a8328eb4c078
damienharper <damien.harper@gmail.com>
2022-12-07 15:52:11 +0100

    Updated PHP-CS-Fixer rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,15 +2,17 @@
 
 declare(strict_types=1);
 
-$config = new PhpCsFixer\Config();
-$config
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+    ->append([__FILE__])
+;
+
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
-        '@PHP74Migration:risky' => true,
-//        '@PHP80Migration' => true,
-//        '@PHP80Migration:risky' => true,
         '@DoctrineAnnotation' => true,
         '@PHPUnit84Migration:risky' => true,
         'date_time_immutable' => true,
@@ -29,7 +31,7 @@ $config
         'method_chaining_indentation' => true,
         'nullable_type_declaration_for_default_null_value' => true,
         'ordered_interfaces' => true,
-        'ordered_traits' => false,
+        'ordered_traits' => true,
         'php_unit_size_class' => true,
         'php_unit_test_class_requires_covers' => false,
         'phpdoc_types' => true,
@@ -39,10 +41,9 @@ $config
         'regular_callable_call' => true,
         'self_static_accessor' => true,
         'simplified_if_return' => true,
-        'simplified_null_return' => false,
+        'simplified_null_return' => true,
         'static_lambda' => true,
+        'get_class_to_class_keyword' => true,
     ])
-    ->setFinder(PhpCsFixer\Finder::create()->in(__DIR__))
+    ->setFinder($finder)
 ;
-
-return $config;
