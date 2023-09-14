29ea0af34755d603c6ace67c2b450e95837fd5d3
a.dmitryuk <a.dmitryuk@movavi.com>
2023-09-14 15:36:41 +0600

    Invalid schema

---
+++ tests/Provider/Doctrine/Issues/Issue174Test.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DH\Auditor\Tests\Provider\Doctrine\Issues;
+
+use DH\Auditor\Provider\Doctrine\Configuration;
+use DH\Auditor\Provider\Doctrine\Persistence\Schema\SchemaManager;
+use DH\Auditor\Tests\Provider\Doctrine\Traits\Schema\DefaultSchemaSetupTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+#[\PHPUnit\Framework\Attributes\Small]
+/**
+ * @internal
+ */
+final class Issue174Test extends TestCase
+{
+    use DefaultSchemaSetupTrait;
+
+    public function testIssue174(): void
+    {
+        $configuration = new Configuration(['table_prefix' => 'audit_', 'table_suffix' => '_suffix']);
+        $manager = new SchemaManager($this->provider);
+        $result = $manager->computeAuditTablename('schema.entity', $configuration);
+        self::assertSame('schema.audit_entity_suffix', $result);
+    }
+}
