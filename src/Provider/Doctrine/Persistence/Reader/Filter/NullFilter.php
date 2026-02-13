ef93df1c257874f101ea572b6c59b697c84b5dee
Damien Harper <damien.harper@gmail.com>
2026-02-13 23:55:51 +0100

    feat(filter): add NullFilter for handling NULL values in queries

---
+++ src/Provider/Doctrine/Persistence/Reader/Filter/NullFilter.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DH\Auditor\Provider\Doctrine\Persistence\Reader\Filter;
+
+/**
+ * Filter for NULL values (e.g., anonymous users where blame_id IS NULL).
+ */
+final readonly class NullFilter implements FilterInterface
+{
+    public function __construct(private string $name) {}
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getSQL(): array
+    {
+        return [
+            'sql' => \sprintf('%s IS NULL', $this->name),
+            'params' => [],
+        ];
+    }
+}
